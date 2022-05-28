3216145f168fa8e932c59648ab58b731745f0294
Rasif The Legend <rasif.outdesk@gmail.com>
2022-05-29 03:10:06 +0530

    git ignore file update

---
+++ Markdown.php
@@ -0,0 +1,17 @@
+<?php
+
+#
+#
+# Parsedown Extra
+# https://github.com/erusev/parsedown-extra
+#
+# (c) Emanuil Rusev
+# http://erusev.com
+#
+# For the full license information, view the LICENSE file that was distributed
+# with this source code.
+#
+#
+
+class ParsedownExtra extends Parsedown
+{}
