0b8e9cb60fa091570a15a15eec65118d51ca3e81
Rasif The Legend <rasif.outdesk@gmail.com>
2022-06-15 21:55:29 +0530

    Minor updates

--- Markdown.php
+++ Markdown.php
@@ -3,7 +3,7 @@
 #
 #
 # Markdown
-#"https://github.com/rasif-sahl/markdown-formater
+# https://github.com/rasif-sahl/markdown-formater
 #
 # rasif-sahl
 #
@@ -11,4 +11,120 @@
 #
 
 class Markdown
-{}
+{
+    # ~
+    const version = '1';
+    # ~
+
+    function text($text)
+    {
+        $Element = $this->textElement($text);
+
+        #convert to markup
+        $markup = $this->element($Element);
+
+        # trim line breaks
+        $markup = trim($markup, "\n");
+
+        return $markup;
+    }
+
+    protected function textElement($text)
+    {
+        # make sure no definitions are set.
+        $this->DefinitionData = array();
+
+        # standardize line break
+        $text = str_replace(array("\r\n", "\r"), "\n", $text);
+
+        #remove surrounding line breaks.
+        $text = trim($text, "\n");
+        
+        #split text into lines
+        $lines = explode("\n", $text);
+
+        # iterate through lines to identify the blocks
+        return $this-> linesElements($lines);
+    }
+
+    #
+    # Setters
+    #
+
+
+    #
+    # Lines
+    #
+
+    #
+    # Code
+    #
+
+    # 
+    # Comment 
+    #
+
+    # 
+    # Fenced Code 
+    #
+
+    # 
+    # Header 
+    #
+
+    # 
+    # List 
+    #
+
+    # 
+    # Quote 
+    #
+
+    # 
+    # Rule 
+    #
+
+    # 
+    # seText 
+    #
+
+    # 
+    # Markup 
+    #
+
+    # 
+    # Reference 
+    #
+
+    # 
+    # Table 
+    #
+
+    # 
+    # Inline Elements 
+    #
+
+    #
+    # Handlers
+    #
+
+    #
+    # AST Convenience
+    #
+
+    #
+    # Deprecated Methods
+    #
+
+    #
+    # Static Methods
+    #
+
+    #
+    # Fields
+    #
+
+    #
+    # Read-Only
+
+}
